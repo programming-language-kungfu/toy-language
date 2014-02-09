210a556341cb175b7a322c66f228f3f81544b931
Kisitu Augustine <austiine04@gmail.com>
2014-02-09 17:45:15 +0300

    refactoring and adding a test for the parser

--- src/test/java/parser/ParserTest.java
+++ src/test/java/parser/ParserTest.java
@@ -1,21 +1,41 @@
 package parser;
 
 import org.junit.Test;
+import parser.ast.IntegerLiteral;
 import parser.ast.PrintStatement;
 import parser.ast.StringLiteral;
+import parser.ast.AssignmentStatement;
 
 import static org.junit.Assert.assertEquals;
 
 public class ParserTest {
 
+    private String sourceCode;
+    private Scanner scanner;
+    private Parser parser;
+
     @Test
     public void testShouldParsePrintSatement() {
-        String sourceCode = "print \"Hello World\"";
-        Scanner scanner = new Scanner(sourceCode);
+        sourceCode = "print \"Hello World\"";
+        scanner = new Scanner(sourceCode);
 
-        Parser parser = new Parser(scanner.getTokens());
+        parser = new Parser(scanner.getTokens());
 
         PrintStatement printStatement = new PrintStatement(new StringLiteral("Hello World"));
         assertEquals(parser.abstractSyntaxTree(), printStatement);
     }
+
+    @Test
+    public void testShouldPrintAssignmentStatement() {
+        sourceCode = "var a = 1";
+        scanner = new Scanner(sourceCode);
+        IntegerLiteral integerLiteral = new IntegerLiteral();
+        integerLiteral.value = 1;
+
+        parser = new Parser(scanner.getTokens());
+
+        assertEquals(parser.abstractSyntaxTree(), new AssignmentStatement("a", integerLiteral));
+    }
+
+
 }
