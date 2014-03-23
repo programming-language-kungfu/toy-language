66e8c6f5bf99ae8bc3532042b2e855de2624b225
Kisitu Augustine <austiine04@gmail.com>
2014-03-23 14:09:55 +0300

    reimplementing parsing for basic print, string and integer assignement statements

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -4,6 +4,7 @@ import parser.ast.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
 public class Parser {
     private final List<String> tokens;
     private Statement abstractSyntaxTree;
@@ -15,50 +16,26 @@ public class Parser {
 
     public Statement abstractSyntaxTree() {
         for (String token : tokens) {
-            try {
-                int currentIndex = tokens.indexOf(token);
-                String nextToken = tokens.get(currentIndex + 1);
-
-                if (token.equals("print")) {
-                    String substring = nextToken.replaceAll("^\" | $\"", "");
-                    if (substring instanceof String) {
-                        abstractSyntaxTree = new PrintStatement(new StringLiteral(nextToken));
-                    }
-                } else if (token.equals("var")) {
-                    String tokenAfterEquals = tokens.get(currentIndex + 3);
-                    String tokenAfterEqualsWithOutQoutes = tokenAfterEquals.replaceAll("^\" | $\" ", "");
-
-                    currentIndex = currentIndex + 4;
-                    if(tokens.get(currentIndex + 4).equals(";") || tokens.get(currentIndex).equals("\n")){
-                        try{
-                            Integer integer = Integer.parseInt(tokenAfterEqualsWithOutQoutes);
-                            abstractSyntaxTree = new AssignmentStatement(nextToken, new IntegerLiteral(integer));
-                        }catch(Exception e){
-                            abstractSyntaxTree = new AssignmentStatement(nextToken, new StringLiteral(tokenAfterEquals));
-                        }
-                    }else {
-                        nextToken = tokens.get(currentIndex);
-                        List<String> operands = new ArrayList<String>();
-                        while(!nextToken.equals(";") && !nextToken.equals("\n")){
-                            operands.add(nextToken);
-                            nextToken = tokens.get(currentIndex ++);
-                        }
-
-
-                        IntegerLiteral leftOperand = new IntegerLiteral(Integer.parseInt(operands.get(0).replace("^\" | $\"", "")));
-
-                        BinaryOperator operator = BinaryOperator.Add;
-
-                        IntegerLiteral rightOperand = new IntegerLiteral(Integer.parseInt(operands.get(2).replace("^\" | $\"", "")));
-                        BinaryExpression binaryExpression = new BinaryExpression(leftOperand, operator, rightOperand);
-                        abstractSyntaxTree = new AssignmentStatement(tokenAfterEquals, binaryExpression);
-                    }
+            int currentIndex = tokens.indexOf(token);
+            if (token.equals("print")) {
+                parsePrintStatementAt(currentIndex);
+                break;
+            } else {
+                try {
+                    Integer number = Integer.parseInt(tokens.get(3));
+                    abstractSyntaxTree = new AssignmentStatement(tokens.get(1), new IntegerLiteral(number));
+                } catch (NumberFormatException nfe) {
+                    abstractSyntaxTree = new AssignmentStatement(tokens.get(1), new StringLiteral(tokens.get(3)));
                 }
-            } catch (IndexOutOfBoundsException aie) {
                 break;
             }
         }
         return abstractSyntaxTree;
+    }
+
+    private void parsePrintStatementAt(int currentIndex) {
+        String expressionToPrint = tokens.get(currentIndex + 1);
+        abstractSyntaxTree = new PrintStatement(new StringLiteral(expressionToPrint));
     }
 
 }
