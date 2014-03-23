d6c11957b01b48aee7ac112c34b23fc32a5a2b7e
Kisitu Augustine <austiine04@gmail.com>
2014-03-23 14:26:04 +0300

    cleaning up the code in the parser

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -15,22 +15,36 @@ public class Parser {
     }
 
     public Statement abstractSyntaxTree() {
+
         for (String token : tokens) {
+
             int currentIndex = tokens.indexOf(token);
+
             if (token.equals("print")) {
                 parsePrintStatementAt(currentIndex);
                 break;
             } else {
-                try {
-                    Integer number = Integer.parseInt(tokens.get(3));
-                    abstractSyntaxTree = new AssignmentStatement(tokens.get(1), new IntegerLiteral(number));
-                } catch (NumberFormatException nfe) {
-                    abstractSyntaxTree = new AssignmentStatement(tokens.get(1), new StringLiteral(tokens.get(3)));
-                }
+                String tokenAfterEquals = tokens.get(currentIndex + 3);
+                String identifier = tokens.get(1);
+
+                parseAssignmentStatementWith(identifier, tokenAfterEquals);
                 break;
             }
         }
         return abstractSyntaxTree;
+    }
+
+    private void parseAssignmentStatementWith(String identifier, String tokenAfterEquals) {
+        try {
+            parseIntegerAssignmentWith(identifier, tokenAfterEquals);
+        } catch (NumberFormatException nfe) {
+            abstractSyntaxTree = new AssignmentStatement(identifier, new StringLiteral(tokenAfterEquals));
+        }
+    }
+
+    private void parseIntegerAssignmentWith(String identifier, String tokenAfterEquals) {
+        Integer number = Integer.parseInt(tokenAfterEquals);
+        abstractSyntaxTree = new AssignmentStatement(identifier, new IntegerLiteral(number));
     }
 
     private void parsePrintStatementAt(int currentIndex) {
